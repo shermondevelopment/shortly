95ab937260ff582ec009af1f99a5c4b2a1398550
shermondevelopment <victorshermon@yahoo.com>
2022-06-08 22:24:38 -0300

    including library cors in project

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 /* express dependencies */
 import express, { json } from 'express'
 
+/** cors  */
+import cors from 'cors'
+
 /** initial server  */
 const app = express()
 
 /** setting server */
 app.use(json())
+app.use(cors())
 
 app.listen(process.env.PORT || 5555, () => console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀🚀`))
