8d9a802b4b03f05173b1ae978cdb4feed354b235
shermondevelopment <victorshermon@yahoo.com>
2022-06-10 12:00:31 -0300

    fix: connect databse

--- app/config/connect-db.js
+++ app/config/connect-db.js
@@ -1,8 +1,16 @@
 import pg from 'pg'
+import 'dotenv/config'
 
 const { Pool } = pg
 
 const connectionString = process.env.DATABASE_URL
+
+if (process.env.MODE === 'PROD') {
+  connectionString.ssl = {
+    rejectUnauthorized: false
+  }
+}
+
 const db = new Pool({ connectionString })
 
 export default db
