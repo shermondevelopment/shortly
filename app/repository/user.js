c2db6a9b8cdb3acf98162048dc8693d6119fa686
pedro imbrech <elprogramadorpedro@gmail.com>
2022-06-22 17:05:50 -0300

    rev

--- app/repository/user.js
+++
@@ -1,9 +0,0 @@
-/** db  */
-import db from '../config/connect-db.js'
-
-export class Users {
-  async findUser(email) {
-    let user = await db.query('select * from users where email = $1', [email])
-    return user
-  }
-}
--- app/utils/error.js
+++
@@ -1,7 +0,0 @@
-export default async (dataValidation, schema) => {
-  try {
-    await schema.validateAsync(dataValidation)
-  } catch (error) {
-    return error.message
-  }
-}
